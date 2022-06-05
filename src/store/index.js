c0cc6cf9e764b8abb6796e3df5e1dfdd0fef02a4
沐雪 <48502416+lisbeth0720@users.noreply.github.com>
2022-06-05 12:25:26 +0800

    实现列表项的添加、修改、删除、选中状态的更改以及展示分类的切换

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,14 @@ import modules from './modules.js'
 
 const state = {
     //所有的任务列表
-    list: []
+    list: [],
+
+    inputValue: '健身',
+
+    //下一个id
+    nextId: 5,
+    //展示的分类-默认展示的是所有的
+    viewKey: 'all'
 };
 
 import { createStore } from 'vuex'
