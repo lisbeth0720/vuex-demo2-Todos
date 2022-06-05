bacfee6db53059954f1932a366bd80011904348f
沐雪 <48502416+lisbeth0720@users.noreply.github.com>
2022-06-05 12:21:57 +0800

    实现列表项的添加、修改、删除、选中状态的更改以及展示分类的切换

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,11 +4,20 @@
 //但action还是通过触发mutation的方式来间接变更数据
 
 import axios from 'axios'
+//import { request } from "../network/request.js";
 
 export default {
     getList(context) {
         axios.get('/list.json').then(res => {
-            context.commit('initList', res)
+            //注意：请求到的res不是需要的数据，res.data才是list.json里的数据
+            context.commit('initList', res.data)
         })
+
+        // return request({
+        //     url: 'list.json'
+        // }).then(res => {
+        //     console.log(res)
+        //     context.commit('initList', res)
+        // })
     }
 }
