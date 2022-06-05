05f0d2317b9f06b15a2452a0b2287b9a078bdb86
沐雪 <48502416+lisbeth0720@users.noreply.github.com>
2022-06-05 12:20:51 +0800

    实现列表项的添加、修改、删除、选中状态的更改以及展示分类的切换

--- src/store/getters.js
+++ src/store/getters.js
@@ -5,5 +5,20 @@
 //1.Getter可以对store中的已有的数据加工处理之后形成新的数据，类似于于计算属性
 //2.store中的数据发生变化，Getter包装后的数据也会跟着变化
 export default {
+    //统计未完成的任务的条数
+    unDoneLength(state) {
+        return state.list.filter(x => x.done === false).length
+    },
 
+    //展示对应的分类下的项
+    infoList(state) {
+        if (state.viekKey == 'all') { //展示所有
+            return state.list;
+        } else if (state.viewKey == 'undone') {
+            return state.list.filter(x => !x.done);
+        } else if (state.viewKey == 'done') {
+            return state.list.filter(x => x.done);
+        }
+        return state.list;
+    }
 }
